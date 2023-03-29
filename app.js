41fcbad7835d87f188e2b25e467cb5c3eadb978b
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-03-29 18:00:20 -0300

    add post method

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
-const app = express()
-const PORT = 3000
 
 const CUBES = require("./data/cubes.json")
+const PORT = 3000
+
+const app = express()
+
+app.use(express.json())
 
 app.get('/v1/cubes', (req, res) => {
-  res.status(200).json({...CUBES})
+  res.status(200).json({
+    status: "success",
+    result: CUBES.cubes.length,
+    data: CUBES.cubes
+  })
+})
+
+app.post("/v1/cubes", (req, res) => {
+
+  const newId = Number(CUBES.cubes[CUBES.cubes.length - 1].id) + 1;
+
+   const newCube = {
+     id: newId,
+     ...req.body
+   }
+   CUBES.cubes.push(newCube)
+
+  res.status(201).json({
+    status: "success",
+    data: newCube
+  })
 })
 
 app.listen(PORT, () => {
